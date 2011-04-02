9d490f07071b580f8d39affb6fb1fe5a80b6c444
Christopher Maujean <cmaujean@gmail.com>
2011-04-01 18:41:52 -0700

    added gitignore file
    * turned Project into a proper thor backed generator
    * added .gitignore file with some default things
    * changed the spec structure a bit to incorporate future feature additions

--- lib/thor/project.rb
+++ lib/thor/project.rb
@@ -1,10 +1,31 @@
-class Project < Thor
+class Project < Thor::Group
   include Thor::Actions
   include Workzone
   
-  desc "create", "Create a new project with git and an rvmrc"
-  def create(name)
-    Dir.mkdir("#{WZ_PROJECT_DIR}/#{name}")
+  argument :name, {
+    :desc => "The name of your new project.",
+    :type => :string,
+    :required => true,
+    :banner => "project_name"
+  }
+
+
+  def create_project_dir
+    empty_directory(project)
   end
-  
+
+  def create_gitignore_file
+    template("project/gitignore.tt", "#{project}/.gitignore")
+  end
+
+  no_tasks do
+    def project
+        @project ||= "#{WZ_PROJECT_DIR}/#{name}"
+        @project
+    end
+  end  
+
+  def self.source_root
+    File.expand_path(File.dirname(__FILE__))
+  end
 end
--- spec/project_create_spec.rb
+++ spec/project_create_spec.rb
@@ -1,12 +1,29 @@
 require 'spec_helper'
 
-describe "thor project:create" do
-  it "creates a directory for the new project in projects" do
-    `thor project create test_project`
-    File.exists?("#{WZ_PROJECT_DIR}/test_project").should be_true
-  end
-  
-  after :each do
-    `rm -rf #{WZ_PROJECT_DIR}/test_project` if File.exists? "#{WZ_PROJECT_DIR}/test_project"
+PROJECT = "#{WZ_PROJECT_DIR}/test_project"
+
+describe "thor tasks" do
+  describe "task: project test_project" do
+    before :each do
+      `thor project test_project`
+    end
+
+    it "creates a directory for the new project in projects" do
+      Dir.exists?(PROJECT).should be_true
+    end
+
+    describe ".gitignore" do
+      it "creates a .gitignore file" do
+        File.exists?("#{PROJECT}/.gitignore").should be_true
+      end
+
+      it "fills the .gitignore file" do
+        File.open("#{PROJECT}/.gitignore").readline.strip.should =~ /\\#*/
+      end
+    end
+
+    after :each do
+      `rm -rf #{WZ_PROJECT_DIR}/test_project` if File.exists? "#{WZ_PROJECT_DIR}/test_project"
+    end
   end
 end
